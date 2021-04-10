edd81edda581b0a1022bfe627063286b5f62a99e
manolo <viprestal@hotmail.com>
2021-04-10 11:34:13 +0200

    Creadas categorias

---
+++ app/Http/Requests/Category/StoreRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:50',
+            'description' => 'nullable|string|max:250'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'El nombre es obligatorio',
+            'name.string' => 'El nombre tiene que ser un texto',
+            'name.max' => 'El nombre no puede supera los 250 caracteres',
+
+            
+            'description.string' => 'La descripción tiene que ser un texto',
+            'description.max' => 'La descripción no puede supera los 250 caracteres',
+        ];
+    }
+}
