767ee0f2a77163d0a8daf2480ed095373c528e4d
Manolo Cabrera <viprestal@hotmail.com>
2021-04-11 13:38:41 +0200

    Preparamdo plantillas

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -3,83 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Models\Purchase;
+use App\Models\Provider;
 use Illuminate\Http\Request;
+use App\Http\Requests\Purchase\StoreRequest;
+use App\Http\Requests\Purchase\UpdateRequest;
 
 class PurchaseController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $purchases = Purchase::get();
+        return view('admin.purchase.index', compact('purchases'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        $providers = Provider::get();
+        return view('admin.purchase.create', compact('providers'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        $purchase = Purchase::create($request->all());
+        
+        foreach ($request->product_id as $key => $product) {
+            $results[] = array("product_id"=>$request->product_id[$key], "quantity"=>$request->quantity[$key], "price"=>$request->price[$key]);
+        }
+        $purchase->purchaseDetails()->createMany($results);
+
+
+        return redirect()->route('purchases.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Purchase  $purchase
-     * @return \Illuminate\Http\Response
-     */
     public function show(Purchase $purchase)
     {
-        //
+        return view('admin.purchase.show', compact('purchase'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Purchase  $purchase
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Purchase $purchase)
     {
-        //
+        $providers = Provider::get();
+        return view('admin.purchase.show', compact('purchase'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Purchase  $purchase
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Purchase $purchase)
+    public function update(UpdateRequest $request, Purchase $purchase)
     {
-        //
+        // $purchase->update($request->all());
+        // return redirect()->route('purchases.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Purchase  $purchase
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Purchase $purchase)
     {
-        //
+        // $purchase->delete();
+        // return redirect()->route('purchases.index');
     }
 }
---
+++ app/Models/Sale.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sale_date',
+        'tax',
+        'total',
+        'status',
+        'client_id',
+        'user_id',
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function saleDetails()
+    {
+        return $this->belongsTo(saleDetail::class);
+    }
+}
